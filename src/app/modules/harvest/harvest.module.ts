67e49bd0a32e07608409eb29c65cd02da2fd8e4b
Ariel Sobrado <asobrado@gmail.com>
2019-04-12 08:26:52 -0300

    componente nuevo de newHarvest

--- src/app/modules/harvest/harvest.module.ts
+++ src/app/modules/harvest/harvest.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListHarvestComponent } from './list-harvest/list-harvest.component';
+import { NewHarvestComponent } from './new-harvest/new-harvest.component';
 
 @NgModule({
-  declarations: [ListHarvestComponent],
+  declarations: [ListHarvestComponent, NewHarvestComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/modules/harvest/new-harvest/new-harvest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-harvest',
+  templateUrl: './new-harvest.component.html',
+  styleUrls: ['./new-harvest.component.scss']
+})
+export class NewHarvestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
