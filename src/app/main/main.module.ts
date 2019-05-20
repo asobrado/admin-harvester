85856708f5792467d312f8d2379411bd4171c212
Ariel Sobrado <asobrado@gmail.com>
2019-05-20 13:30:58 -0300

    se creo modulo main

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+
+import { LoginModule } from 'app/main/authentication/login/login.module';
+import { RegisterModule } from 'app/main/authentication/register/register.module';
+import { ForgotPasswordModule } from 'app/main/authentication/forgot-password/forgot-password.module';
+import { ResetPasswordModule } from 'app/main/authentication/reset-password/reset-password.module';
+import { LockModule } from 'app/main/authentication/lock/lock.module';
+import { MailConfirmModule } from 'app/main/authentication/mail-confirm/mail-confirm.module';
+import { Error404Module } from 'app/main/errors/404/error-404.module';
+import { Error500Module } from 'app/main/errors/500/error-500.module';
+
+@NgModule({
+    imports: [
+        // Authentication
+        LoginModule,
+        RegisterModule,
+        ForgotPasswordModule,
+        ResetPasswordModule,
+        LockModule,
+        MailConfirmModule,
+
+        // Errors
+        Error404Module,
+        Error500Module,
+    ]
+})
+export class PagesModule
+{
+
+}
